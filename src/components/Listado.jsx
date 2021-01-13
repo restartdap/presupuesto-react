fae0d3e012cbb2606df8ede9396b95ea63f1d3d6
Rafael Estrada <restradap1600@gmail.com>
2021-01-12 20:53:13 -0600

    Se desarrolló la aplicación de presupuesto

---
+++ src/components/Listado.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Gasto from './Gasto';
+import PropTypes from "prop-types";
+
+const Listado = ({gastos}) => (
+    <div className="gastos-realizados">
+        <h2>Listado</h2>
+        <ul>
+        {
+            gastos.map(gasto => (
+                <Gasto
+                    key={gasto.id}
+                    gasto={gasto}
+                />
+            ))
+        }
+        </ul>
+    </div>
+);
+
+Listado.propTypes = {
+    gastos: PropTypes.array.isRequired
+}
+
+export default Listado;
